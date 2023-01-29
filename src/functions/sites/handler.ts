c2b117a0f1b0d430e5d56a27c018c5d4b150bc92
Gary <garymuir17@gmail.com>
2023-01-29 21:34:52 +0000

    removes some dead variables

--- src/functions/sites/handler.ts
+++ src/functions/sites/handler.ts
@@ -1,21 +1,28 @@
 import { APIGatewayProxyHandler } from 'aws-lambda'
-import axios from 'axios'
 import conn from '@libs/db'
 
 import { corsSuccessResponse, corsErrorResponse } from '@libs/lambda-response'
 
-export const main: APIGatewayProxyHandler = async (event) => {
-    console.log('banana hello from sites')
-    const params = event.queryStringParameters
+export const main: APIGatewayProxyHandler = async () => {
     try {
         const query = await conn.query(
             `select json_build_object(
                 'type', 'FeatureCollection',
                 'features', json_agg(feature)
-              ) polygons
+            ) polygons
               from 
-            (select ST_AsGeoJson(rs.wkb_geometry)::json feature
-            from roman_sites rs 
+            (select  json_build_object(
+                'id', rs.pid,
+                'type', 'Feature',
+                'geometry', (ST_TRANSFORM(rs.wkb_geometry, 4326))::json,
+                'properties', json_build_object(
+                    'class', rs.class,
+                    'type', rs.type,
+                    'link', rs.link,
+                    'area', rs.shape_area
+                )
+            ) feature
+            from roman_sites rs
             ) t1`
         )
         const { polygons } = query.rows[0]
